c833a1bd1cf2869cc68d67ceac2a39fc64e674b9
Dharmit Shah <shahdharmit@gmail.com>
2013-12-04 23:26:49 +0530

    Removed original Numbers/prime.py and coded new one

--- Numbers/next_prime.py
+++ Numbers/next_prime.py
@@ -1,25 +1,33 @@
-# Next Prime Number - Have the program find prime
-# numbers until the user chooses to stop asking for
-# the next one.
+#!/usr/bin/env python
+import math
+n = 2
 
-def next_prime(current):
-    next_prime = current + 1 # start checking for primes 1 number after the current one
-    i = 2
-    while next_prime > i: # check with numbers up to next_prime - 1
-        if next_prime % i == 0: # if number is divisible
-            next_prime += 1 # ready to check the next number
-            i = 2 # reset i to check divisibility again from 2
+
+def get_prime():
+    global n
+    isPrime = True
+
+    if n == 2:
+        n += 1
+        return 2
+    else:
+        for i in range(2, int(math.sqrt(n))+1):
+            if n % i == 0:
+                isPrime = False
+                break
+        if isPrime:
+            prime = n
+            n += 1
+            return prime
         else:
-            i += 1 # increment the divisor
-    return next_prime
+            n += 1
+            return get_prime()
 
-if __name__ == '__main__':
-    current_prime = 2
-    while True:
-        response = raw_input('Do you want the next prime? (Y/N) ')
 
-        if response.lower().startswith('y'):
-            print current_prime
-            current_prime = next_prime(current_prime)
-        else:
-            break
+if __name__ == "__main__":
+    next = 'y'
+    while next != 'n':
+        next = raw_input("Enter 'n' to stop generating prime numbers: ")
+        prime = get_prime()
+        if prime:
+            print prime
