65a49217f78eb07f9faa225b1ad043bd51b0f51a
Dharmit Shah <shahdharmit@gmail.com>
2013-12-05 23:36:23 +0530

    Numbers/change.py modified to comply with PEP-8

--- Numbers/change.py
+++ Numbers/change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-# Change Return Program - The user enters a cost and then the amount of money 
-# given. The program will figure out the change and the number of quarters, 
+# Change Return Program - The user enters a cost and then the amount of money
+# given. The program will figure out the change and the number of quarters,
 # dimes, nickels, pennies needed for the change.
 # Coding this as per Indian currency
 
@@ -14,57 +14,61 @@ def change(cost, payment):
     thousand = c / 1000
     c = c - (thousand * 1000)
     if c == 0:
-        d = {1000:thousand}
+        d = {1000: thousand}
         return d
-    
+
     five_hundred = c / 500
     c = c - (five_hundred * 500)
     if c == 0:
-        d = {1000:thousand, 500:five_hundred}
+        d = {1000: thousand, 500: five_hundred}
         return d
 
     hundred = c / 100
     c = c - (hundred * 100)
     if c == 0:
-        d = {1000:thousand, 500:five_hundred, 100:hundred}
+        d = {1000: thousand, 500: five_hundred, 100: hundred}
         return d
 
     fifty = c / 50
     c = c - (fifty * 50)
     if c == 0:
-        d = {1000:thousand, 500:five_hundred, 100:hundred, 50:fifty}
+        d = {1000: thousand, 500: five_hundred, 100: hundred, 50: fifty}
         return d
 
     twenty = c / 20
     c = c - (twenty * 20)
     if c == 0:
-        d = {1000:thousand, 500:five_hundred, 100:hundred, 50:fifty, 20:twenty}
+        d = {1000: thousand, 500: five_hundred, 100: hundred, 50: fifty,
+             20: twenty}
         return d
 
     ten = c / 10
     c = c - (ten * 10)
     if c == 0:
-        d = {1000:thousand, 500:five_hundred, 100:hundred, 50:fifty, 20:twenty, 10:ten}
+        d = {1000: thousand, 500: five_hundred, 100: hundred, 50: fifty,
+             20: twenty, 10: ten}
         return d
 
     five = c / 5
     c = c - (five * 5)
     if c == 0:
-        d = {1000:thousand, 500:five_hundred, 100:hundred, 50:fifty, 20:twenty, 10:ten, 5:five}
+        d = {1000: thousand, 500: five_hundred, 100: hundred, 50: fifty,
+             20: twenty, 10: ten, 5: five}
         return d
 
     two = c / 2
-    c = c- (two * 2)
-    d = {1000:thousand, 500:five_hundred, 100:hundred, 50:fifty, 20:twenty, 10:ten, 5:five, 2:two, 1:c}
+    c = c - (two * 2)
+    d = {1000: thousand, 500: five_hundred, 100: hundred, 50: fifty,
+         20: twenty, 10: ten, 5: five, 2: two, 1: c}
     return d
 
 if __name__ == "__main__":
     cost = int(raw_input("Enter the cost price: "))
     payment = int(raw_input("Enter the amount paid: "))
-    
+
     print "Change to be returned is: "
     pay = change(cost, payment)
-    
+
     amt = 0
     for i in sorted(pay.iterkeys()):
         if pay[i] != 0:
